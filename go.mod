268de033e85f4817abfc2a02053e62ec2d8633fb
Thean <theantian089@gmail.com>
2024-01-17 19:24:33 +0700

    import dependancies

--- go.mod
+++ go.mod
@@ -1,4 +1,4 @@
-module github.com/Thean-Arch/go_bookstore
+module github.com/Thean-Arch/go_library
 
 go 1.21.5
 
---
+++ pkg/routes/bookstore_route.go
@@ -0,0 +1,6 @@
+package routes
+
+import (
+	"gihub.com/Thean-Arch/go_library/pkg/controllers"
+	"github.com/gorilla/mux"
+)
